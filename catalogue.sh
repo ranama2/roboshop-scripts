97a278887c07029a1fba9b4719ece816b73f212d
Meherun Mithila <mnm19322@email.vccs.edu>
2025-02-17 01:03:47 -0500

    catalogue service file and shell script added

---
+++ catalogue.sh
@@ -0,0 +1,22 @@
+
+
+cp catalogue.service /etc/systemd/system/catalogue.service
+
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+
+dnf install nodejs -y
+
+useradd roboshop
+
+mkdir /app
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+cd /app
+unzip /tmp/catalogue.zip
+npm install
+
+systemctl reload-daemon
+
+systemctl enable catalogue
+systemctl start catalogue
